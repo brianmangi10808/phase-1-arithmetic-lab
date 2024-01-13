fced3cbdc17c9f86a17cba18351263d6bb2dfde0
Your Name <brianmwangi10808@gmail.com>
2024-01-13 16:25:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1= 31;
+let num2= 2;
+const multiply =num1 *num2;
+console.log(multiply)
+
+
+
+let random = Math.floor(Math.random()*10) +1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 4;
+let mod = num3/num4;
+console.log(mod);
+
+
+let max = Math.max(5,10,15,20);
+console.log(max);
+
